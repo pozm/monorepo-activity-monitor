543c8600b57311942080e166c9e85d10d27198bb
Luna <44528100+pozm@users.noreply.github.com>
2022-07-04 00:56:32 +0100

    fixes to user page

--- web/frontend/src/pages/user.tsx
+++ web/frontend/src/pages/user.tsx
@@ -1,6 +1,6 @@
 import { formatDuration, intervalToDuration, sub } from "date-fns";
 import { useParams } from "solid-app-router";
-import { createMemo, createResource, For } from "solid-js";
+import { createMemo, createResource, For, Match, Show, Suspense, Switch } from "solid-js";
 import { base_path } from "../../config";
 import { Activity, User } from "../types";
 
@@ -32,22 +32,30 @@ export default function UserPage() {
 
     return (
         <div class="flex flex-col  p-20 text-gray-300">
-            <img src="" />
-            <h1 class="text-gray-200 font-semibold text-5xl first-letter:uppercase" >{user()?.name}'s</h1>
-            <h3 class="font-medium text-xl" >Activities</h3>
+            <Suspense fallback={"loading..."} >
 
-            <div class="flex w-full flex-wrap flex-row justify-around" >
-                <For each={user()?.activities}>{activity => 
-                    <div class="mb-2 border-solid rounded-xl border-zinc-700 border-2 p-5 bg-zinc-900 shadow-xl flex flex-col" >
-                        <img src={`${base_path}api/user/${user()?.name}/img/${activity.name}`} class="self-center" width="64" />
-                        <h2 class="text-xl font-medium first-letter:uppercase self-center" >
-                            {activity.name}
-                        </h2>
-                        <p>Time Spent: <span class="text-pink-300 w-full " >{getTimeSpent(activity)}</span></p>
-                        <p>Active Now : <span class="text-pink-300" >{activity.active ? "Yes" : "No"}</span></p>
+            <Switch fallback={<div>User Not Found</div>} >
+                <Match when={user()?.ID} >
+                    <img src="" />
+                    <h1 class="text-gray-200 font-semibold text-5xl first-letter:uppercase" >{`${user()?.name ?? "unknown"}'s`}</h1>
+                    <h3 class="font-medium text-xl pb-4" >Activities</h3>
+
+                    <div class="flex w-full flex-wrap flex-row justify-around" >
+                        <For each={user()?.activities}>{activity => 
+                            <div class="mb-2 border-solid rounded-xl border-zinc-700 border-2 p-5 bg-zinc-900 shadow-xl flex flex-col" >
+                                <img src={`${base_path}api/user/${user()?.name}/img/${activity.name}`} class="self-center" width="64" />
+                                <h2 class="text-xl font-medium first-letter:uppercase self-center" >
+                                    {activity.name}
+                                </h2>
+                                <p>Time Spent: <span class="text-pink-300 w-full " >{getTimeSpent(activity)}</span></p>
+                                <p>Active Now : <span class="text-pink-300" >{activity.active ? "Yes" : "No"}</span></p>
+                            </div>
+                        }</For>
                     </div>
-                }</For>
-            </div>
+                </Match>
+            </Switch>
+
+            </Suspense>
         </div>
     )
 }
